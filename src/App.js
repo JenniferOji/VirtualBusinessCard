d8fd5aa615ac66a08de29dd930994f08b912c538
JENNIFER OJI <G00413791@atu.ie>
2025-02-19 19:46:33 +0000

    base project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Profile from './components/Profile';
+import SignUp from './components/SignUp';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/profile" element={<Profile />}/>
+        <Route path="/signUp" element={<SignUp />}/>
+      </Routes>  
+
+      <Profile></Profile> 
+      <SignUp></SignUp>   
+    </Router>
   );
 }
 
