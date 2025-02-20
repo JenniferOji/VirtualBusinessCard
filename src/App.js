c58faf9c0e168a1f282d0e663296db7cf25fce17
JENNIFER OJI <G00413791@atu.ie>
2025-02-20 18:28:48 +0000

    added a navigation bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Profile from './components/Profile';
 import SignUp from './components/SignUp';
+import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <Navigation />
       <Routes>
         <Route path="/profile" element={<Profile />}/>
         <Route path="/signUp" element={<SignUp />}/>
       </Routes>  
-      <SignUp></SignUp>   
     </Router>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Navigation = () => {
+  return (
+        // the theme of the naviagtion bar 
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            {/* clicking virtual card brings you to the loading screen of the app */}
+            <Navbar.Brand href="../App" >Virtual Card</Navbar.Brand>
+            <Nav className="me-auto">
+                {/* the routes present on the navigation bar */}
+              <Nav.Link href="/SignUp">Sign Up</Nav.Link>
+              <Nav.Link href="/Profile">Profile</Nav.Link>
+            </Nav>
+          </Container>
+      </Navbar>
+  );
+};
+
+export default Navigation;
