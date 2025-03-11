792ee1f12fbab52d05912ac7efd35c0ff9396c05
JENNIFER OJI <G00413791@atu.ie>
2025-03-11 20:19:13 +0000

    Created the Navigation component. Refs #3

---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Navigation = () => {
+  return (
+        // the theme of the naviagtion bar 
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            {/* clicking virtual card text brings you to the loading screen of the app */}
+            <Navbar.Brand href="../App" >Virtual Business Card</Navbar.Brand>
+            <Nav className="me-auto">
+                {/* the routes present on the navigation bar */}
+              <Nav.Link href="/Login">Sign Up</Nav.Link>
+              <Nav.Link href="/SignUp">Log In</Nav.Link>
+            </Nav>
+          </Container>
+      </Navbar>
+  );
+};
+
+export default Navigation;
