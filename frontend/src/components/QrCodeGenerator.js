c2f12b07e74d3b4f45040bd6f132242ae18667a6
JENNIFER OJI <G00413791@atu.ie>
2025-03-23 20:03:37 +0000

    Made the Qr Code downloadable. Refs #9

--- frontend/src/components/QrCodeGenerator.js
+++ frontend/src/components/QrCodeGenerator.js
@@ -2,12 +2,28 @@ import React, { useState, useRef } from 'react';
 import QRCode from 'react-qr-code';
 import { BASE_URL } from '../config';
 import { useParams } from 'react-router-dom';
+import * as htmlToImage from "html-to-image"; // library to generate images from HTML elements 
 
 // https://dev.to/onlyoneerin/creating-dynamic-qr-codes-using-reactjs-a-step-by-step-tutorial-341a
 function QrCodeGenerator() {
     const { id } = useParams(); // pulling the id from the url 
     const [url] = useState(`${BASE_URL}/professional/portfolio/${id}`); // the qr code will be generater from this url 
     const qrCodeRef = useRef(null);
+
+    // function to download the QR code 
+    const downloadQRCode = () => {
+        htmlToImage
+            .toPng(qrCodeRef.current) // converting the QR code to a png image 
+            .then(function (dataUrl) { // creating a download link 
+                const link = document.createElement("a");
+                link.href = dataUrl;
+                link.download = "QR Code.png"; // the name of the download 
+                link.click();
+            })
+            .catch(function (error) {
+                console.error("Error generating the QR code:", error);
+            });
+    };
 
     return (
       <div className="qrcode_container">
@@ -19,6 +35,7 @@ function QrCodeGenerator() {
                 {/* the qr code generated from the url */}
                 <QRCode value={url} size={300} ref={qrCodeRef} /> 
               </div>
+              <button onClick={downloadQRCode}>Download Qr Code</button>
             </div>
         </div>
     );
